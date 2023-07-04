353665ff312be71cbee4f8539d71a134eea3ac47
Lynn Cohen <lcohen0010@gmail.com>
2023-07-04 17:02:17 -0400

    added demos

---
+++ force-app/main/default/staticresources/PD1/filterWords.js
@@ -0,0 +1,13 @@
+function filterWords(elementId) {
+    const descText = document.getElementById(elementId).value;
+    alert('debug> description text: ' + descText);
+    const descArray = descText.split(" ");
+    alert('debug> broken into array: ' + JSON.stringify(descArray))
+    const words = ['maybe', 'should', 'think', 'like', 'very'];
+    const filteredArray = descArray.filter(value => words.includes(value));
+    if (filteredArray.length == 0)
+        alert('Congratulations, no weak or overused words found')
+    else
+        alert('Consider editing your text content. Found the following weak or overused words: ' + JSON.stringify(filteredArray));
+    
+}
