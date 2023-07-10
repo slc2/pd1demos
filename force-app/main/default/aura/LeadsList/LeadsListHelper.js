d2e0647e42922ec6c3678b4551ae3528425f5521
Lynn Cohen <lcohen0010@gmail.com>
2023-07-10 10:07:42 -0400

    fixed styling

--- force-app/main/default/aura/LeadsList/LeadsListHelper.js
+++ force-app/main/default/aura/LeadsList/LeadsListHelper.js
@@ -1,29 +1,21 @@
 ({
     loadLeads : function(cmp) {
-         // Load all contact data, calling apex controller
          var action = cmp.get("c.getLeads");
          action.setCallback(this, function(response) {
              var state = response.getState();
              if (state === "SUCCESS") {
                  cmp.set("v.leads", response.getReturnValue());
                  this.updateTotal(cmp);
-             }
- 
-             // Display toast message to indicate load status
-             var toastEvent = $A.get("e.force:showToast");
-             if (state === 'SUCCESS'){
+             }        
+             
+             if (state !== 'SUCCESS'){
+                var toastEvent = $A.get("e.force:showToast");
                  toastEvent.setParams({
-                     "title": "Success!",
-                     "message": " Your leads have been loaded successfully."
+                     "title": "Error Loading Leads",
+                     "message": "A server error occurred."
                  });
-             }
-             else {
-                 toastEvent.setParams({
-                         "title": "Error!",
-                         "message": " Something has gone wrong."
-                 });
-             }
-             toastEvent.fire();
+                 toastEvent.fire();
+             }             
          });
           $A.enqueueAction(action);
      },
