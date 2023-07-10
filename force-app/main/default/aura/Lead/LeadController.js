6e1356cdf7f71c5b8f365cd2de8c2ac23bd6760a
Lynn Cohen <lcohen0010@gmail.com>
2023-07-10 10:23:54 -0400

    cleanup and styling

--- force-app/main/default/aura/Lead/LeadController.js
+++ force-app/main/default/aura/Lead/LeadController.js
@@ -3,7 +3,6 @@
         helper.getPicklistValues(component, event);
     },
     goToRecord : function(component, event, helper) {
-        // Fire the event to navigate to the contact record
         var sObjectEvent = $A.get("e.force:navigateToSObject");
         sObjectEvent.setParams({
             "recordId": component.get("v.lead.Id")
